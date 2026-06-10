afb4601af726514257960ddda1283056906d366c
Sarvjeet Singh <sarvjeet.singh.work@gmail.com>
2026-06-10 17:34:42 +0530

    Live Hosting Websites with Git

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+
+  base: '/sarvjeet-portfolio/', 
   plugins: [
     tailwindcss(),
   ],
