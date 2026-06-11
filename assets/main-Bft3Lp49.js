6f19daa1f08010499e7f9d81419558bc56f1ff91
Sarvjeet Singh <sarvjeet.singh.work@gmail.com>
2026-06-12 02:36:34 +0530

    Updates

--- assets/main-Bft3Lp49.js
+++ assets/main-Bft3Lp49.js
@@ -3,44 +3,33 @@
     <!-- Hamburger Icon -->
     <button id="btn-hamburger" class="px-2 rounded-full cursor-pointer active:scale-95 bg-gray-800/40 hover:bg-green-800 transition-all duration-300"><i class="ri-menu-5-line"></i></button>
 
-    <a href="/sarvjeet-portfolio/index.html" id="web-logo" class="text-lg md:text-3xl font-bold leading-tight">
+    <a href="/sarvjeet-portfolio/" id="web-logo" class="text-lg md:text-3xl font-bold leading-tight">
       Sarvjeet.dev
     </a>
    </div>
 
-    <div class="flex justify-center items-center gap-5">
-      <div id="hamburger-menu" class="absolute mt-6 top-full left-0 w-full opacity-0 invisible -translate-y-3 transition-all duration-300 ease-in-out">
-        <ul class="
-        border
-        border-gray-600 bg-black 
-        w-full 
-        mx-auto rounded-lg
-        gray-600 flex 
-        flex-col items-center 
-        gap-4 py-4">
-          <li><a href="/sarvjeet-portfolio/index.html" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Home</a></li>
-          <li><a href="/sarvjeet-portfolio/index#about" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">About</a></li>
-          <li><a href="/sarvjeet-portfolio/index#skills" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Skills</a></li>
-          <li><a href="/sarvjeet-portfolio/index#engineering-process" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Engineering Process</a></li>
-          <li><a href="/sarvjeet-portfolio/index#featured-projects" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Featured Projects</a></li>
-          <li><a href="/sarvjeet-portfolio/index#contact" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Contact</a></li>
-        </ul>
-      </div>
+  <div class="flex justify-center items-center gap-5">
+    <div id="hamburger-menu" class="absolute mt-6 top-full left-0 w-full opacity-0 invisible -translate-y-3 transition-all duration-300 ease-in-out">
+      <ul class="
+      border
+      border-gray-600 bg-black 
+      w-full 
+      mx-auto rounded-lg
+      gray-600 flex 
+      flex-col items-center 
+      gap-4 py-4">
+        <li><a href="/sarvjeet-portfolio/" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Home</a></li>
+        <li><a href="/sarvjeet-portfolio/#about" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">About</a></li>
+        <li><a href="/sarvjeet-portfolio/#skills" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Skills</a></li>
+        <li><a href="/sarvjeet-portfolio/#engineering-process" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Engineering Process</a></li>
+        <li><a href="/sarvjeet-portfolio/#featured-projects" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Featured Projects</a></li>
+        <li><a href="/sarvjeet-portfolio/#contact" class="rounded-xl px-5 py-1 hover:bg-green-800 transition-all duration-300">Contact</a></li>
+      </ul>
+    </div>
 
-    <a
-  href="/sarvjeetResume.pdf"
-  download
-  class="
-    text-sm md:text-base
-    px-4 py-2
-    rounded-full
-    bg-green-500/40
-    hover:bg-green-800
-    transition-all duration-300
-  "
->
-  Download CV
-</a>
+    <a href="/sarvjeet-portfolio/sarvjeetResume.pdf" download class="text-sm md:text-basepx-4 py-2 rounded-full bg-green-500/40 hover:bg-green-800 transition-all duration-300>
+      Download CV
+    </a>
       </div>
    `,t=()=>`
   <section id="hero-section-left" class="flex flex-col justify-center items-center text-center w-full gap-5 h-full px-5">
@@ -553,7 +542,7 @@
         </div>`).join(``);return`
         <div class="flex flex-col justify-center items-center w-full gap-3">
             <div class="w-full flex md:px-6 py-2">
-                <a href="/index.html#featured-projects" class="border border-gray-500 px-4 py-1 rounded-full text-sm text-gray-400 hover:bg-green-800 hover:text-white transition-all duration-300">
+                <a href="/sarvjeet-portfolio/#featured-projects" class="border border-gray-500 px-4 py-1 rounded-full text-sm text-gray-400 hover:bg-green-800 hover:text-white transition-all duration-300">
                     <- Back to Projects 
                 </a>
             </div>
